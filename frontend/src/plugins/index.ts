d157b49f5f633beb698e18bbe09e98b76bcf1a26
Mariusz Franciszczak <mariusz.franciszczak@gmail.com>
2025-08-15 17:34:26 +0200

    fix: Directly initialize Vuetify and Vue Router in main.ts

--- frontend/src/plugins/index.ts
+++
@@ -1,9 +0,0 @@
-import type { App } from 'vue'
-import router from '../router'
-import vuetify from './vuetify'
-
-export function registerPlugins(app: App) {
-  app
-    .use(vuetify)
-    .use(router)
-}
