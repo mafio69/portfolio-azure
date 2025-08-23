04a4fc53309c9c67c10b6a339ca1fc45b3369d92
Mariusz Franciszczak <mariusz.franciszczak@gmail.com>
2025-08-23 23:11:14 +0200

    FIX frontend ,Rewrite the file nginx.conf, startup.sh

--- startup.sh
+++ startup.sh
@@ -1,18 +1,14 @@
 #!/usr/bin/env bash
 set -euo pipefail
-
-# Podmień konfigurację Nginx
 cp /home/site/wwwroot/nginx.conf /etc/nginx/nginx.conf
-
-# Uruchom php-fpm w tle (nazwa binarki zależna od środowiska)
-if command -v php-fpm8.2 >/dev/null 2>&1; then
+if command -v php-fpm8.3 >/dev/null 2>&1; then
   php-fpm8.3 -D
 elif command -v php-fpm >/dev/null 2>&1; then
   php-fpm -D
 else
-  echo "Brak php-fpm w środowisku. Użyj planu/obrazu z PHP-FPM lub zmień stack."
-  exit 1
+  echo "Brak php-fpm 8.3 w środowisku"; exit 1
 fi
 
-# Uruchom Nginx w foreground (dla App Service)
-exec nginx -g 'daemon off;'
+chmod +x deploy_package/startup.sh
+
+exec nginx -g 'daemon off;'c
