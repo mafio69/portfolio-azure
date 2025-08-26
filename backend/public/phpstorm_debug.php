170b7910f50a2fe9a8f0ffe77cf62cb42890c229
Mariusz Franciszczak <mariusz.franciszczak@gmail.com>
2025-08-26 16:51:14 +0200

    fix: backend  index.php

--- backend/public/phpstorm_debug.php
+++
@@ -1,5 +0,0 @@
-<?php
-include 'phar://phpstorm_debug_validator.phar/common.php';
-
-$content = XDebugValidator::run();
-echo $content;
--- backend/public/phpstorm_index.php
+++
@@ -1,2 +0,0 @@
-<?php
-require "phpstorm_debug_validator.phar";
