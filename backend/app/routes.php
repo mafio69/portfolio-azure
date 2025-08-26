864e135146b4dcbd1229443b8a1ec50965f458d7
Mariusz Franciszczak <mariusz.franciszczak@gmail.com>
2025-08-26 16:36:41 +0200

    fix: backend  routes.php

--- backend/app/routes.php
+++ backend/app/routes.php
@@ -1,17 +1,18 @@
 <?php
-// Obsługa preflight OPTIONS requests
-$app->options('/{routes:.+}', function ($request, $response, $args) {
-    return $response;
-});
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
 
-// Twoje istniejące trasy...
-$app->get('/api/projects', function ($request, $response) {
-    $data = [
-        ['id' => 1, 'name' => 'Projekt 1', 'description' => 'Opis 1', 'url' => 'http://example.com', 'technologies' => ['PHP', 'Vue.js']],
-        ['id' => 2, 'name' => 'Projekt 2', 'description' => 'Opis 2', 'url' => 'http://example2.com', 'technologies' => ['JavaScript', 'Node.js']]
-    ];
+return function ($app) {
+    $app->options('/{routes:.+}', function (Request $request, Response $response, $args) {
+        return $response;
+    });
 
-    return $response
-        ->withHeader('Content-Type', 'application/json')
-        ->getBody()->write(json_encode($data));
-});
+    $app->get('/api/projects', function (Request $request, Response $response, $args) {
+        $data = [
+            ['id' => 1, 'name' => 'Projekt 1', 'description' => 'Opis 1', 'url' => 'http://example.com', 'technologies' => ['PHP', 'Vue.js']],
+            ['id' => 2, 'name' => 'Projekt 2', 'description' => 'Opis 2', 'url' => 'http://example2.com', 'technologies' => ['JavaScript', 'Node.js']]
+        ];
+        $response->getBody()->write(json_encode($data));
+        return $response->withHeader('Content-Type', 'application/json');
+    });
+};
