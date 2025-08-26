6e9f9a41bef3cad96095eb2ba7b49e5fe3d90614
Mariusz Franciszczak <mariusz.franciszczak@gmail.com>
2025-08-26 17:09:03 +0200

    fix: backend routes.php .php

--- backend/app/routes.php
+++ backend/app/routes.php
@@ -3,15 +3,18 @@ use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
 return function ($app) {
-    $app->options('/{routes:.+}', function (Request $request, Response $response, $args) {
+    // OPTIONS dla CORS
+    $app->options('/{routes:.+}', function (Request $request, Response $response) {
         return $response;
     });
 
-    $app->get('/api/projects', function (Request $request, Response $response, $args) {
+    // API endpoint
+    $app->get('/api/projects', function (Request $request, Response $response) {
         $data = [
             ['id' => 1, 'name' => 'Projekt 1', 'description' => 'Opis 1', 'url' => 'http://example.com', 'technologies' => ['PHP', 'Vue.js']],
             ['id' => 2, 'name' => 'Projekt 2', 'description' => 'Opis 2', 'url' => 'http://example2.com', 'technologies' => ['JavaScript', 'Node.js']]
         ];
+
         $response->getBody()->write(json_encode($data));
         return $response->withHeader('Content-Type', 'application/json');
     });
